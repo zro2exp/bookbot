0996b6e10cb7d4b7d0698579401a7c55aa3e153e
zro2exp <zro2exp@proton.me>
2025-01-25 21:42:43 -0600

    add main.py functionality

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        words = file_contents.split()
-        print(len(words))
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    print(chars_dict)
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
 
 main()
