e69f266ba27c9c21823f0d83198f0f8037f9ecee
zro2exp <zro2exp@proton.me>
2025-01-25 22:18:35 -0600

    add print report functionality

--- main.py
+++ main.py
@@ -4,7 +4,17 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
-    print(chars_dict)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    print()
+
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item["char"]}' character was found {item["count"]} times")
+    print()
+    print("--- End report ---")
 
 def get_num_words(text):
     words = text.split()
@@ -19,6 +29,16 @@ def get_chars_dict(text):
         else:
             chars[lowered] = 1
     return chars
+
+def sort_on(dict):
+    return dict["count"]
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "count": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
 
 def get_book_text(path):
     with open(path) as f:
